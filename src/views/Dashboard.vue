c62ad3f1f52a57012ae710cbc396a219e878d9fc
Michael Schaap <michael_schaap@outlook.com>
2023-01-08 23:13:19 +0100

    feat(Tableau Embedding): implemented

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="page-content">
-    <h2>Test</h2>
+    <h2>Dashboards</h2>
+    <tableau-viz id="tableauViz"></tableau-viz>
+
+
   </div>
 </template>
 
 <script>
+import { TableauEventType } from '@/assets/js/tableau.embedding.3.2.0.min.js';
 
 export default {
   name: 'TheDashboard',
-  methods: {}
+  mounted() {
+    this.initViz();
+  },
+  methods: {
+    initViz() {
+      let viz = document.getElementById("tableauViz");
+      viz.width = 1200;
+      viz.width = 800;
+      viz.hideTabs = true;
+      viz.hideToolbar = true;
+
+      const onFirstInteractive = () => {
+        let workbook = viz.getWorkbook();
+        workbook.getActiveSheet();
+      };
+
+      viz.addEventListener(TableauEventType.FirstInteractive, onFirstInteractive);
+      viz.src = "https://dub01.online.tableau.com/t/portfolio3/views/Geregistreerdevoertuigenelektrischenelektriciteitsprijzen/Geregistreerdevoertuigenelektrischenelektriciteitsprijzen";
+    }
+  }
 }
 
 </script>
