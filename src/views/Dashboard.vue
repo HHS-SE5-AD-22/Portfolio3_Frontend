6354661b174a933657aca3c2b17fb2e2e0913518
Michael Schaap <michael_schaap@outlook.com>
2023-01-16 22:55:17 +0100

    fix(build): done

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -64,7 +64,7 @@ export default {
       switch (tabNumber) {
         case 1:
           viz = document.getElementById('tableauDescriptive');
-          src = "https://dub01.online.tableau.com/t/hhsbigdata/views/Descriptivelijndiagram2/Staafdiagram";
+          src = "https://dub01.online.tableau.com/t/hhsbigdata/views/DescriptiveDiagram/Staafdiagram";
           break;
         case 2:
           viz = document.getElementById('tableauDiagnostic');
